4a08a70a214c63ff29509145034cff48b0e63d23
Matias <mrodriguezbegueri@gmail.com>
2023-09-10 17:58:39 -0300

    fix favicon and add players icon instead text

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-"use client"
+"use client";
 
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Item } from "@/components";
 import PlayerColumn from "@/components/PlayerColumn";
 import { useContext } from "react";
 import { GameContext } from "@/context";
-
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 
 export default function Home() {
-  const { players } = useContext(GameContext)
+  const { players } = useContext(GameContext);
 
   return (
-    <div style={{ display: 'flex' }}>
-      <Grid container spacing={2} >
+    <div style={{ display: "flex" }}>
+      <Grid container spacing={2}>
         <Grid rowSpacing={2} container item xs={2}>
           <Grid item xs={12}>
-            <Item>Jugadores</Item>
+            <Item sx={{ alignItems: 'center' }}>
+              <PeopleAltIcon />
+            </Item>
           </Grid>
           <Grid item xs={12}>
             <Item>1</Item>
@@ -55,11 +57,9 @@ export default function Home() {
           </Grid>
         </Grid>
 
-        {
-          players.map((player) => (
-            <PlayerColumn player={player} countPlayers={players.length} key={player.name} />
-          ))
-        }
+        {players.map((player) => (
+          <PlayerColumn player={player} countPlayers={players.length} key={player.name} />
+        ))}
       </Grid>
     </div>
   );
