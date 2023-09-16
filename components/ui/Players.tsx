4ad2338e471556252f43f3f3cde6a0b07f1f5373
Matias <mrodriguezbegueri@gmail.com>
2023-09-16 20:54:06 -0300

    sticky grid

--- components/ui/Players.tsx
+++ components/ui/Players.tsx
@@ -7,38 +7,43 @@ import { Item } from "..";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import { Player } from "@/interfaces";
 
-import styles from './players.module.css'
+import styles from "./players.module.css";
 
 const Players = () => {
   const { players, setCurrentPlayer, currentPlayer } = useContext(GameContext);
 
-    const handleOnClickPlayer = (player: Player) => {
-        setCurrentPlayer(player)
-    }
+  const handleOnClickPlayer = (player: Player) => {
+    setCurrentPlayer(player);
+  };
 
-    const isCurrentPlayer = (player: Player): boolean => {
-        return currentPlayer.name === player.name
-    }
+  const isCurrentPlayer = (player: Player): boolean => {
+    return currentPlayer.name === player.name;
+  };
 
-    return (
-    <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
-      <Grid rowSpacing={2} container item xs={4}>
-        <Grid item xs={12}>
-          <Item sx={{ alignItems: "center" }}>
-            <PeopleAltIcon />
-          </Item>
-        </Grid>
-      </Grid>
-      {players.map((player) => (
-        <Grid rowSpacing={2} container item xs={Math.max(8 / players.length)} key={player.name}>
-          <Grid  item xs={12}>
-              <Item className={ isCurrentPlayer(player) ? styles.currentPlayer : ''}  onClick={() => handleOnClickPlayer(player)}>
-                <Typography>{player.name}</Typography>
-              </Item>
+  return (
+    <div style={{ position: 'sticky',  top: "1rem", zIndex: 1 }}>
+      <Grid container spacing={2} sx={{ marginBottom: "20px" }}>
+        <Grid rowSpacing={2} container item xs={4}>
+          <Grid item xs={12}>
+            <Item sx={{ alignItems: "center" }}>
+              <PeopleAltIcon />
+            </Item>
           </Grid>
         </Grid>
-      ))}
-    </Grid>
+        {players.map((player) => (
+          <Grid rowSpacing={2} container item xs={Math.max(8 / players.length)} key={player.name}>
+            <Grid item xs={12}>
+              <Item
+                className={isCurrentPlayer(player) ? styles.currentPlayer : ""}
+                onClick={() => handleOnClickPlayer(player)}
+              >
+                <Typography>{player.name}</Typography>
+              </Item>
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 };
 
