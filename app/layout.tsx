049b2c2ab8e1107c4a98c78407f144c0cc4f39bd
Matias <mrodriguezbegueri@gmail.com>
2023-09-16 18:10:13 -0300

    add AppBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import Providers from "./providers";
 import { Box } from "@mui/material";
 import { GameProvider } from "@/context";
+import { NavBar } from "@/components/ui";
 
 const roboto = Roboto({
   weight: "700",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <GameProvider>
         <body className={roboto.className}>
           <Providers>
+            <NavBar/>
             <main>
               <Box sx={{ flexFlow: 1 }}>
                 <Box sx={{ padding: "10px 20px" }}>{children}</Box>
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Players } from './Players'
 export * from './Board'
 export { default as TableScore } from './table-score/TableScore'
+export { default as NavBar } from './nav-bar/NavBar'
