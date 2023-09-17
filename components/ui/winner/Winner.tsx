c4eb10f2a5e007ba43e36f537004a72464548551
Matias <mrodriguezbegueri@gmail.com>
2023-09-17 02:50:59 -0300

    test deploy

--- components/ui/winner/Winner.tsx
+++ components/ui/winner/Winner.tsx
@@ -1,46 +1,44 @@
-"use client"
+"use client";
 
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { Player } from '@/interfaces';
-import { FC, useState } from 'react';
-import { Typography } from '@mui/material';
-import { useRouter } from 'next/navigation';
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Player } from "@/interfaces";
+import { FC, useState } from "react";
+import { Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 interface Props {
-    winnerPlayer: Player | null
+  winnerPlayer: Player | null;
 }
 
- const WinnerDialog:FC<Props> =  ({ winnerPlayer }) => {
- const [open, setOpen] = useState(true)
+const WinnerDialog: FC<Props> = ({ winnerPlayer }) => {
+  const [open, setOpen] = useState(true);
 
-  const router = useRouter()
+  const router = useRouter();
 
   const handleClose = () => {
-    router.push('/')
+    router.push("/");
     setOpen(false);
   };
 
   return (
     <div>
-      {
-        winnerPlayer !== null ? 
+      {winnerPlayer !== null ? (
         <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Felicitaciones!!!</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <Typography variant='h3'>
-            El Ganador es {winnerPlayer.name}!!!
-            </Typography>
-          </DialogContentText>
-        </DialogContent>
-      </Dialog>
-      : <></>
-      }
+          <DialogTitle>Felicitaciones!!!</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <Typography variant="h3">El Ganador es {winnerPlayer.name}!!!</Typography>
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+      ) : (
+        <></>
+      )}
     </div>
   );
-}
+};
 
-export default WinnerDialog
+export default WinnerDialog;
