994e47d5184c1cc12371dfb4b556645dacbb17a2
Matias <mrodriguezbegueri@gmail.com>
2024-01-26 00:25:40 -0300

    fix scale

--- components/Item.tsx
+++ components/Item.tsx
@@ -7,13 +7,16 @@ const Item = styled(Paper)(({ theme }) => ({
   flexDirection: "column",
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(2),
+  padding: theme.spacing(1.5),
   textAlign: "center",
   justifyContent:"center",
   color: theme.palette.text.secondary,
-  height: "100px",
-  overflowWrap: "break-word",
-  textOverflow: "ellipsis"
+  height: "100%",
+  width: "100%",
+  overflow: "hidden",
+  [theme.breakpoints.down('sm')]: {
+    padding: theme.spacing(0.5)
+  },
 }));
 
 export default Item;
--- components/ui/Board.tsx
+++ components/ui/Board.tsx
@@ -1,44 +1,96 @@
-import { Grid } from "@mui/material";
+"use client";
+import { Grid, Typography } from "@mui/material";
+import { Textfit } from "react-textfit";
 import { Item } from "..";
+
 
 export const Board = () => {
   return (
     <Grid wrap="wrap" rowSpacing={2} container item xs={3}>
       <Grid item xs={12}>
-        <Item>1</Item>
+        
+          <Textfit  mode="single">
+          <Item>
+            <Typography>1</Typography>
+        </Item>
+          </Textfit>
       </Grid>
       <Grid item xs={12}>
-        <Item>2</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>2</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>3</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>3</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>4</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>4</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>5</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>5</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>6</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>6</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>Escalera</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>Escalera</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>Full</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>Full</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>Poker</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>Poker</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>Generala</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>Generala</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>Doble</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>Doble</Typography>
+          </Textfit>
+        </Item>
       </Grid>
       <Grid item xs={12}>
-        <Item>Total</Item>
+        <Item>
+          <Textfit  mode="single">
+            <Typography>Total</Typography>
+          </Textfit>
+        </Item>
       </Grid>
     </Grid>
   );
